b6fe80489cb471f579b8c475a96cdfd5d44a10dc
Kiefer Wiessler <Kiefer@MacBook-Pro-Kiefer.local>
2015-11-02 18:05:32 +0100

    ft_is MAJ

---
+++ ft_isdigit.c
@@ -0,0 +1,9 @@
+#include "test.h"
+
+int		ft_isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
---
+++ test.h
@@ -0,0 +1,8 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+
+#endif
