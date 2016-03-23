e0d9bb74e8d5dcb30c4a5611c747d110b620a684
kwiessle <kiefer@iMac-de-Kiefer.local>
2016-03-23 17:56:03 +0100

    ft_strdup fix

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: kwiessle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/14 18:53:51 by kwiessle          #+#    #+#             */
-/*   Updated: 2015/12/17 13:51:32 by kwiessle         ###   ########.fr       */
+/*   Updated: 2016/03/23 17:55:35 by kwiessle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s1)
 	char		*s2;
 
 	i = 0;
-	s2 = malloc(sizeof(char) * ft_strlen(s1));
+	s2 = malloc(sizeof(char) * ft_strlen(s1) + 1);
 	if (!s1 || !s2)
 		return (NULL);
 	while (s1[i] != '\0')
